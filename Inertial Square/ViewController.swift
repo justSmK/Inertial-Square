0cd31020ec8a6fc35878aed5f604da6f243e94e1
justSmK <28645140+justSmK@users.noreply.github.com>
2023-05-16 13:27:56 +0300

    Init project

---
+++ Inertial Square/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inertial Square
+//
+//  Created by Sergei Semko on 5/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
